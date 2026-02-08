237667cf02eba3fe6553748ef005603660701c07
malaka96 <madhubhashana655@gmail.com>
2026-02-09 00:17:25 +0530

    setuped basic routers

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-
+import { Route, Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import ResetPassword from "./pages/ResetPassword";
+import EmailVerify from "./pages/EmailVerify";
 
 const App = () => {
   return (
-    <div>
-      <h1>Welcome to Authify</h1>
-    </div>
-  )
-}
+    <>
+      <ToastContainer />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/reset-password" element={<ResetPassword />}></Route>
+        <Route path="/email-verify" element={<EmailVerify />}></Route>
+      </Routes>
+    </>
+  );
+};
 
-export default App
+export default App;
