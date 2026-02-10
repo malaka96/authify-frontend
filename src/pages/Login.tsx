f0466a8e687441f75a226e345949c5de3e95d587
malaka96 <madhubhashana655@gmail.com>
2026-02-10 19:08:07 +0530

    updated login page to toggle with create account

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,14 +1,27 @@
+import { useState } from "react";
 import { FaSignInAlt } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 const Login = () => {
+  const [isCreatedAccount, setIsCreatedAccount] = useState<boolean>(false);
+
   return (
     <div className="flex items-center justify-center min-h-screen bg-black text-white px-6">
       <div className="w-full max-w-md bg-gray-900 rounded-lg shadow-lg p-8">
-
         <h2 className="text-3xl font-bold text-red-500 mb-6 text-center">
-          Login to Authify
+          {isCreatedAccount ? "Login to Authify" : "Create Account"}
         </h2>
+
+        {!isCreatedAccount ? (
+          <div className="mb-4">
+            <label className="block text-gray-300 mb-2">Full Name</label>
+            <input
+              type="text"
+              placeholder="Enter your full name"
+              className="w-full px-4 py-2 rounded-md bg-gray-800 text-white border border-gray-700 focus:outline-none focus:ring-2 focus:ring-red-500"
+            />
+          </div>
+        ) : null}
 
         <div className="mb-4">
           <label className="block text-gray-300 mb-2">Email</label>
@@ -22,12 +35,14 @@ const Login = () => {
         <div className="mb-6">
           <div className="flex items-center justify-between mb-2">
             <label className="text-gray-300">Password</label>
-            <Link
-              to="/reset-password"
-              className="text-sm text-red-500 hover:underline"
-            >
-              Forgot Password?
-            </Link>
+            {isCreatedAccount ? (
+              <Link
+                to="/reset-password"
+                className="text-sm text-red-500 hover:underline"
+              >
+                Forgot Password?
+              </Link>
+            ) : null}
           </div>
           <input
             type="password"
@@ -36,16 +51,56 @@ const Login = () => {
           />
         </div>
 
+        {!isCreatedAccount ? 
+        <div className="mb-6">
+          <div className="flex items-center justify-between mb-2">
+            <label className="text-gray-300">Confirm Password</label>
+            {isCreatedAccount ? (
+              <Link
+                to="/reset-password"
+                className="text-sm text-red-500 hover:underline"
+              >
+                Forgot Password?
+              </Link>
+            ) : null}
+          </div>
+          <input
+            type="password"
+            placeholder="Re-Enter your password"
+            className="w-full px-4 py-2 rounded-md bg-gray-800 text-white border border-gray-700 focus:outline-none focus:ring-2 focus:ring-red-500"
+          />
+        </div> : null}
+
         <button className="w-full flex items-center justify-center gap-2 bg-red-600 hover:bg-red-700 text-white px-4 py-2 rounded-md font-semibold transition duration-200">
           <FaSignInAlt />
           <span>Login</span>
         </button>
 
         <p className="mt-6 text-center text-gray-400">
-          Don’t have an account?{" "}
-          <Link to="/create-account" className="text-red-500 hover:underline">
-            Create Account
-          </Link>
+          {isCreatedAccount
+            ? "Don’t have an account?"
+            : "Already have an account?"}{" "}
+          {isCreatedAccount ? (
+            <Link
+              to="/login"
+              className="text-red-500 hover:underline"
+              onClick={() => {
+                setIsCreatedAccount(false);
+              }}
+            >
+              Create Account
+            </Link>
+          ) : (
+            <Link
+              to="/login"
+              className="text-red-500 hover:underline"
+              onClick={() => {
+                setIsCreatedAccount(true);
+              }}
+            >
+              Login
+            </Link>
+          )}
         </p>
       </div>
     </div>
