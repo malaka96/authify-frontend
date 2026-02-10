68b68a7d323bbeff0f96b50c442f436e65b5d4e7
malaka96 <madhubhashana655@gmail.com>
2026-02-10 18:07:37 +0530

    created nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,12 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import ResetPassword from "./pages/ResetPassword";
 import EmailVerify from "./pages/EmailVerify";
+import Menubar from "./components/Menubar";
 
 const App = () => {
   return (
     <>
+      <Menubar />
       <ToastContainer />
       <Routes>
         <Route path="/" element={<Home />}></Route>
--- src/components/Menubar.tsx
+++ src/components/Menubar.tsx
@@ -1,11 +1,26 @@
-
+import { FaSignInAlt } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 
 const Menubar = () => {
-  return (
-    <div>
-      Menubar Component
-    </div>
-  )
-}
 
-export default Menubar
+  const navigation = useNavigate();
+
+  return (
+    <nav className="flex items-center justify-between px-6 py-4 bg-[#000000e1] text-white">
+      {/* Left side - Brand */}
+      <div className="text-2xl font-bold text-red-500">
+        Authify
+      </div>
+
+      {/* Right side - Login button */}
+      <button className="flex items-center gap-2 bg-red-600 hover:bg-red-700 text-white px-4 py-2 rounded-md transition duration-200"
+        onClick={() =>{navigation("/login")}}
+      >
+        <FaSignInAlt />
+        <span>Login</span>
+      </button>
+    </nav>
+  );
+};
+
+export default Menubar;
