111dfc92ff62b4953121de741fa1024976530a24
malaka96 <madhubhashana655@gmail.com>
2026-02-13 02:51:34 +0530

    completed user registration

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,9 @@
-import { useState } from "react";
+import axios from "axios";
+import { useContext, useState } from "react";
 import { FaSignInAlt } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { AppContext } from "../context/AppContext";
+import { toast } from "react-toastify";
 
 const Login = () => {
   const [isCreatedAccount, setIsCreatedAccount] = useState<boolean>(true);
@@ -9,9 +12,43 @@ const Login = () => {
   const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
 
-  const onSubmitHandler = async (e: React.FormEvent<HTMLFormElement> ) =>{
+  const { backendURL } = useContext(AppContext)!;
+  const navigate = useNavigate();
+
+  const onSubmitHandler = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-  }
+    axios.defaults.withCredentials = true;
+    setLoading(true);
+    try {
+      if (!isCreatedAccount) {
+        // register API
+        const response = await axios.post(`${backendURL}/register`, {
+          name,
+          email,
+          password,
+        });
+
+        if (response.status === 201) {
+          navigate("/");
+          toast.success("Account created successfully");
+        } else {
+          toast.error(
+            "Email already exists or cannot create account this time",
+          );
+        }
+      } else {
+        // login API
+      }
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        toast.error(error.response?.data?.message || "Request failed");
+      } else {
+        toast.error("Unexpected error occurred");
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-black text-white px-6">
@@ -22,108 +59,122 @@ const Login = () => {
 
         <form onSubmit={onSubmitHandler}>
           {!isCreatedAccount ? (
+            <div className="mb-4">
+              <label className="block text-gray-300 mb-2">Full Name</label>
+              <input
+                type="text"
+                id="fullName"
+                placeholder="Enter your full name"
+                onChange={(e) => setName(e.target.value)}
+                value={name}
+                className="w-full px-4 py-2 rounded-md bg-gray-800 text-white border border-gray-700 focus:outline-none focus:ring-2 focus:ring-red-500"
+              />
+            </div>
+          ) : null}
+
           <div className="mb-4">
-            <label className="block text-gray-300 mb-2">Full Name</label>
+            <label className="block text-gray-300 mb-2">Email</label>
             <input
-              type="text"
-              id="fullName"
-              placeholder="Enter your full name"
-              onChange={(e) => setName(e.target.value)}
-              value={name}
+              type="email"
+              id="email"
+              required
+              placeholder="Enter your email"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
               className="w-full px-4 py-2 rounded-md bg-gray-800 text-white border border-gray-700 focus:outline-none focus:ring-2 focus:ring-red-500"
             />
           </div>
-        ) : null}
 
-        <div className="mb-4">
-          <label className="block text-gray-300 mb-2">Email</label>
-          <input
-            type="email"
-            id="email"
-            required
-            placeholder="Enter your email"
-            onChange={(e) => setEmail(e.target.value)}
-            value={email}
-            className="w-full px-4 py-2 rounded-md bg-gray-800 text-white border border-gray-700 focus:outline-none focus:ring-2 focus:ring-red-500"
-          />
-        </div>
+          <div className="mb-6">
+            <div className="flex items-center justify-between mb-2">
+              <label className="text-gray-300">Password</label>
+              {isCreatedAccount ? (
+                <Link
+                  to="/reset-password"
+                  className="text-sm text-red-500 hover:underline"
+                >
+                  Forgot Password?
+                </Link>
+              ) : null}
+            </div>
+            <input
+              type="password"
+              id="password"
+              required
+              placeholder="Enter your password"
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+              className="w-full px-4 py-2 rounded-md bg-gray-800 text-white border border-gray-700 focus:outline-none focus:ring-2 focus:ring-red-500"
+            />
+          </div>
 
-        <div className="mb-6">
-          <div className="flex items-center justify-between mb-2">
-            <label className="text-gray-300">Password</label>
+          {!isCreatedAccount ? (
+            <div className="mb-6">
+              <div className="flex items-center justify-between mb-2">
+                <label className="text-gray-300">Confirm Password</label>
+                {isCreatedAccount ? (
+                  <Link
+                    to="/reset-password"
+                    className="text-sm text-red-500 hover:underline"
+                  >
+                    Forgot Password?
+                  </Link>
+                ) : null}
+              </div>
+              <input
+                type="password"
+                placeholder="Re-Enter your password"
+                className="w-full px-4 py-2 rounded-md bg-gray-800 text-white border border-gray-700 focus:outline-none focus:ring-2 focus:ring-red-500"
+              />
+            </div>
+          ) : null}
+
+          <button
+            type="submit"
+            className="w-full flex items-center justify-center gap-2 bg-red-600 hover:bg-red-700 text-white px-4 py-2 rounded-md font-semibold transition duration-200"
+          >
+            <FaSignInAlt />
+            <span>
+              {loading
+                ? "Processing..."
+                : isCreatedAccount
+                  ? "Login"
+                  : "Create Account"}
+            </span>
+          </button>
+
+          <p className="mt-6 text-center text-gray-400">
+            {isCreatedAccount
+              ? "Don’t have an account?"
+              : "Already have an account?"}{" "}
             {isCreatedAccount ? (
               <Link
-                to="/reset-password"
-                className="text-sm text-red-500 hover:underline"
+                to="/login"
+                className="text-red-500 hover:underline"
+                onClick={() => {
+                  setIsCreatedAccount(false);
+                  setName("");
+                  setEmail("");
+                  setPassword("");
+                }}
               >
-                Forgot Password?
+                Create Account
               </Link>
-            ) : null}
-          </div>
-          <input
-            type="password"
-            id="password"
-            required
-            placeholder="Enter your password"
-            onChange={(e) => setPassword(e.target.value)}
-            value={password}
-            className="w-full px-4 py-2 rounded-md bg-gray-800 text-white border border-gray-700 focus:outline-none focus:ring-2 focus:ring-red-500"
-          />
-        </div>
-
-        {!isCreatedAccount ? 
-        <div className="mb-6">
-          <div className="flex items-center justify-between mb-2">
-            <label className="text-gray-300">Confirm Password</label>
-            {isCreatedAccount ? (
+            ) : (
               <Link
-                to="/reset-password"
-                className="text-sm text-red-500 hover:underline"
+                to="/login"
+                className="text-red-500 hover:underline"
+                onClick={() => {
+                  setIsCreatedAccount(true);
+                  setName("");
+                  setEmail("");
+                  setPassword("");
+                }}
               >
-                Forgot Password?
+                Login
               </Link>
-            ) : null}
-          </div>
-          <input
-            type="password"
-            placeholder="Re-Enter your password"
-            className="w-full px-4 py-2 rounded-md bg-gray-800 text-white border border-gray-700 focus:outline-none focus:ring-2 focus:ring-red-500"
-          />
-        </div> : null}
-
-        <button
-        type="submit"
-         className="w-full flex items-center justify-center gap-2 bg-red-600 hover:bg-red-700 text-white px-4 py-2 rounded-md font-semibold transition duration-200">
-          <FaSignInAlt />
-          <span>{isCreatedAccount ? "Login" : "Create Account"}</span>
-        </button>
-
-        <p className="mt-6 text-center text-gray-400">
-          {isCreatedAccount
-            ? "Don’t have an account?"
-            : "Already have an account?"}{" "}
-          {isCreatedAccount ? (
-            <Link
-              to="/login"
-              className="text-red-500 hover:underline"
-              onClick={() => {
-                setIsCreatedAccount(false);
-              }}
-            >
-              Create Account
-            </Link>
-          ) : (
-            <Link
-              to="/login"
-              className="text-red-500 hover:underline"
-              onClick={() => {
-                setIsCreatedAccount(true);
-              }}
-            >
-              Login
-            </Link>
-          )}
-        </p>
+            )}
+          </p>
         </form>
       </div>
     </div>
--- src/utils/Constants.tsx
+++ src/utils/Constants.tsx
@@ -1,3 +1,3 @@
 export const AppConstants = {
-    API_BASE_URL: 'http://localhost:8080/',
+    API_BASE_URL: 'http://localhost:8080/api/v1.0',
 }
